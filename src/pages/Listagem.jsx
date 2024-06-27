2d3ff5ad58b67f72dd79c597cfd3bdb19ec1f03c
Koezin25 <kauevictor031207@gmail.com>
2024-06-27 09:50:58 -0300

    rs

---
+++ src/pages/Listagem.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import React, {useState, useEffect} from "react"
+
+const Listagem = () => {
+
+    return (
+        <>
+          <h2>Pizzas ITB</h2>
+        </>
+    )
+}
+
+export default Listagem
